d420ef5bd6695420c73585f1894d465f04224765
Cao Mai <caomai@rocketmail.com>
2020-06-03 09:10:56 -0700

    [create] project folder

---
+++ openWeatherAPI/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  openWeatherAPI
+//
+//  Created by Cao Mai on 6/3/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
