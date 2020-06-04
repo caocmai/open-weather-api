f0acc8ee2603c1b48beb7194b3a82b47c62c147d
Cao Mai <caomai@rocketmail.com>
2020-06-03 21:31:32 -0700

    [add] ability for user to input a location

--- openWeatherAPI/Controller/ViewController.swift
+++ openWeatherAPI/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
         
     let network = NetworkLayer()
     
@@ -19,19 +19,52 @@ class ViewController: UIViewController {
         return newImage
     }()
     
+    let textField: UITextField = {
+        let newTextField = UITextField()
+        newTextField.placeholder = "Weather City"
+        newTextField.autocapitalizationType = .words
+        newTextField.returnKeyType = .go
+        newTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        newTextField.translatesAutoresizingMaskIntoConstraints = false
+        return newTextField
+    }()
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        var properSearchQuery = ""
+               for letter in textField.text! {
+                   if letter == " " {
+                       properSearchQuery += "%20" // To deal with space
+                   }else {
+                       properSearchQuery += String(letter)
+                   }
+               }
+        
+        fetchWeather(city: properSearchQuery)
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
 //        fetchPokemonList(url: url)
+        textField.delegate = self
         view.addSubview(image)
+        view.addSubview(textField)
         
         NSLayoutConstraint.activate([
-            image.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            image.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            image.heightAnchor.constraint(equalToConstant: 200),
+            image.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        NSLayoutConstraint.activate([
+            textField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
 
-        image.image = UIImage(systemName: "cloud.fill")
 //        image.image = UIImage(named: "test")
         fetchWeather(city: "Paris")
         view.backgroundColor = .white
@@ -52,7 +85,9 @@ class ViewController: UIViewController {
                 let weather = WeatherModel(conditionId: weatherId, cityName: (data?.name)!, temperature: (data?.main?.temp)!)
                 
                 print(weather.getTemp)
-                print(weather.conditionName)
+//                print(weather.conditionName)
+                self.image.image = UIImage(systemName: weather.conditionName)
+
             case let .failure(someError):
                 print(someError)
             }
